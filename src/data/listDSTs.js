3c5eaa5a140601790f7efd20471a39658667a115
joicy kelly <joicykelly2301@gmail.com>
2024-10-07 21:38:09 -0300

    ✨feat: adding data drunk

---
+++ src/data/listDSTs.js
@@ -0,0 +1,96 @@
+export const listDSTs = [
+    {
+        "idDisease": "d1a2b3c4-e5f6-7890-ab12-cdef34567890",
+        "name": "Sífilis",
+        "type": "Bacteriana",
+        "symptoms": [
+            "Feridas nos órgãos genitais, ânus ou boca",
+            "Eruções cutâneas",
+            "Febre",
+            "Mal-estar geral"
+        ],
+        "transmission": [
+            "Relações sexuais sem proteção",
+            "Contato direto com as feridas",
+            "De mãe para filho durante a gravidez"
+        ],
+        "prevention": [
+            "Uso de preservativos",
+            "Realização de exames regulares",
+            "Tratamento pré-natal adequado"
+        ],
+        "treatment": [
+            "Antibióticos (principalmente penicilina)"
+        ]
+    },
+    {
+        "idDisease": "d2b3c4d5-f678-90ab-12cd-ef34567890ab",
+        "name": "Gonorreia",
+        "type": "Bacteriana",
+        "symptoms": [
+            "Dor ao urinar",
+            "Secreção anormal dos órgãos genitais",
+            "Dor durante relações sexuais",
+            "Inflamação na garganta (casos orais)"
+        ],
+        "transmission": [
+            "Relações sexuais sem proteção",
+            "Contato com secreções infectadas"
+        ],
+        "prevention": [
+            "Uso de preservativos",
+            "Exames regulares",
+            "Educação sexual"
+        ],
+        "treatment": [
+            "Antibióticos (geralmente ceftriaxona e azitromicina)"
+        ]
+    },
+    {
+        "idDisease": "d3c4d5e6-7890-ab12-cdef-34567890ab12",
+        "name": "Clamídia",
+        "type": "Bacteriana",
+        "symptoms": [
+            "Secreção vaginal ou peniana",
+            "Dor ao urinar",
+            "Sangramento fora do período menstrual",
+            "Dor durante relações sexuais"
+        ],
+        "transmission": [
+            "Relações sexuais sem proteção",
+            "Contato com secreções infectadas"
+        ],
+        "prevention": [
+            "Uso de preservativos",
+            "Exames regulares",
+            "Educação sexual"
+        ],
+        "treatment": [
+            "Antibióticos (geralmente azitromicina ou doxiciclina)"
+        ]
+    },
+    {
+        "idDisease": "d4e5f678-90ab-12cd-ef34-567890ab12cd",
+        "name": "Herpes Genital",
+        "type": "Viral",
+        "symptoms": [
+            "Bolhas dolorosas nos órgãos genitais ou ao redor",
+            "Coceira",
+            "Febre",
+            "Dor ao urinar"
+        ],
+        "transmission": [
+            "Contato direto com as lesões durante o sexo",
+            "Contato pele com pele infectada"
+        ],
+        "prevention": [
+            "Uso de preservativos",
+            "Evitar contato sexual durante surtos",
+            "Exames regulares"
+        ],
+        "treatment": [
+            "Antivirais (aciclovir, valaciclovir)",
+            "Tratamento dos sintomas"
+        ]
+    }
+];
