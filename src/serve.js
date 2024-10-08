53a71313fa1ebcf2872edb7cea28d257f35d2340
hewelbelmonte <130703340+hewelbelmonte@users.noreply.github.com>
2024-10-08 20:31:48 -0300

    serve.js outside folder

--- src/serve.js
+++ src/serve.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const localRoutes = require('../routes/local.router.js');
+const localRoutes = require('./routes/local.router.js');
 
 const app = express();
 const PORT = 3000;
