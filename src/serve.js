3f9aa8136718060eb5180c72741554683865c787
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2024-10-08 20:35:12 -0300

    Duplicate server.js deleted

--- src/serve.js
+++ src/serve.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { diseaseRouter } from "./routes/router.js";
+import { userRouter } from "./routes/user.routes.js";
 
 const app = express();
 const port = 3000;
@@ -8,8 +9,12 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 app.use(diseaseRouter);
+app.use(userRouter)
 
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`);
 });
+
+
+
 
