ab11ee2b2eed359b8425607aa1407a72ea9e5523
Rodrigoatila09 <154346800+Rodrigoatila09@users.noreply.github.com>
2024-10-10 09:05:32 -0300

    merge feature/api4 into Developer

--- src/serve.js
+++ src/serve.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { diseaseRouter } from "./routes/disease.routes.js";
 import { userRouter } from "./routes/user.routes.js";
+import { casesRouter } from "./routes/router.js";
 import router from "./routes/local.router.js";
 
 const app = express();
@@ -16,9 +17,10 @@ app.get('/', (req, res) => {
 
 // app.use('/api/locations', localRoutes);
 // app.use(localR)
-app.use(router)
+app.use(router);
 app.use(diseaseRouter);
 app.use(userRouter);
+app.use(casesRouter); 
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
